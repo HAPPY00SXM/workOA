f130c1c5f6219b6f6201596938007b6a88c9bc8d
HAPPY00SXM <happy_sxm@icloud.com>
2026-08-04 17:13:25 +0800

    Create settings.gradle.kts

---
+++ .github/workflows/settings.gradle.kts
@@ -0,0 +1,15 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "WorkCalendar"
+include(":app")包含(":app")
