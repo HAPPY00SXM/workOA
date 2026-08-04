ff1c2cbb9ea85929e36714e1b6565756d197fbdb
HAPPY00SXM <happy_sxm@icloud.com>
2026-08-04 16:28:29 +0800

    Delete oatuijin directory

--- oatuijin/settings.gradle.kts
+++
@@ -1,9 +0,0 @@
-kotlin
-pluginManagement {
-    repositories { google(); mavenCentral(); gradlePluginPortal() }
-}
-dependencyResolutionManagement {
-    repositories { google(); mavenCentral() }
-}
-rootProject.name = "WorkCalendar"
-include(":app")
